d8bd8135d5e8b867538e54bc493606746fef0119
embanhanh <dinhnhuthong59@gmail.com>
2025-08-17 21:02:39 +0700

    First commit: init project

---
+++ README.md
@@ -0,0 +1,29 @@
+.
+├── .husky/
+├── dist/
+├── node_modules/
+├── src/
+│ ├── auth/ # Module xác thực
+│ │ ├── dto/
+│ │ ├── auth.controller.ts
+│ │ ├── auth.module.ts
+│ │ ├── auth.service.ts
+│ │ └── jwt.strategy.ts
+│ ├── users/ # Module người dùng
+│ │ ├── dto/
+│ │ ├── entities/
+│ │ │ └── user.entity.ts
+│ │ ├── users.controller.ts
+│ │ ├── users.module.ts
+│ │ └── users.service.ts
+│ ├── app.controller.ts
+│ ├── app.module.ts
+│ ├── app.service.ts
+│ └── main.ts
+├── .env
+├── .eslintrc.js
+├── .prettierrc
+├── docker-compose.yml
+├── Dockerfile
+├── package.json
+└── tsconfig.json
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Cấu hình Swagger
+  const config = new DocumentBuilder()
+    .setTitle('My Professional API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addBearerAuth() // Thêm nút "Authorize" để test API cần token
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // Endpoint là /api-docs
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Tự động loại bỏ các thuộc tính không được định nghĩa trong DTO
+      forbidNonWhitelisted: true, // Ném lỗi nếu có thuộc tính không mong muốn
+      transform: true, // Tự động chuyển đổi payload sang kiểu DTO
+    }),
+  );
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+}
+bootstrap();
