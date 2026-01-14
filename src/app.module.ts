ce359562af3a39096ee06fdf0d252be255f7d265
embanhanh <dinhnhuthong59@gmail.com>
2026-01-14 19:39:44 +0700

    feat: add schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -98,6 +98,7 @@ const BullModuleTyped = BullModule as unknown as {
         database: configService.get<string>('DB_DATABASE'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true, // Chỉ dùng trong development, tự động tạo table.
+        schema: 'public',
         ssl: configService.get<string>('DB_SSL') === 'true',
         extra:
           configService.get<string>('DB_SSL') === 'true'
